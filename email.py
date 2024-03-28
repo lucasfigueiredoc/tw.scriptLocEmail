85277ca98a0aae89b829dd2d367fc8226d3a6141
Lucas F <luck.camara1@gmail.com>
2024-03-28 07:49:48 -0300

    inital commit

--- email.py
+++ email.py
@@ -4,6 +4,34 @@ import pandas as pd
 table = pd.read_csv('./csvFiles/Acao_e-mail_UG_COB_2203.csv', sep=";")
 
 emails = table['(E-mail) - E-mail']
+emailsT = pd.DataFrame(columns=['E-mail', 'rec'])
+cont = 0
+for email in emails:
+    findedA = email.find('@')
+    
+    if findedA != -1:
+        findedP = email.find('.', findedA)
+        sub = pd.email[findedA:findedP]
+        
+        if findedP != -1: 
+            if sub in emailsT['E-mail']:
+                df.loc['']
+                
+            else:
+                cont += 1
+                emailsT.append(sub)
+            
+df = pd.DataFrame(emailsT)
+            
+df.to_csv('teste.csv')
+
+
+import os
+import pandas as pd
+
+table = pd.read_csv('./csvFiles/Acao_e-mail_UG_COB_2203.csv', sep=";")
+
+emails = table['(E-mail) - E-mail']
 emailsT = [ ]
 cont = 0
 for email in emails:
@@ -23,4 +51,3 @@ for email in emails:
             
 df = pd.DataFrame(emailsT)
             
-df.to_csv('teste.csv')
